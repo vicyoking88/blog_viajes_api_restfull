1890581a5aa3387e652352d949f01f5d002dae7c
vmedina <vamedinac@icloud.com>
2021-01-15 20:50:10 -0500

    api_todas_publi_busque

--- app_web/routes/apis.js
+++ app_web/routes/apis.js
@@ -13,6 +13,26 @@ var pool = mysql.createPool({
     database: 'blog_viajes'
 })
 
+/**api me devuelve todas las publicaciones y nos busca la publicacion que contena las palabras del query string */
+router.get('/api/v1/publicaciones', (request, response) => {
+    pool.getConnection((error, connection) => {
+        const search = (request.query.busqueda) ? request.query.busqueda : "";
+        if (search == "") {
+            query = `SELECT*FROM publicaciones`
+        } else {
+            query = `SELECT*FROM publicaciones WHERE 
+            titulo LIKE '%${search}%' or
+            contenido LIKE '%${search}%' or
+            resumen LIKE '%${search}%'`
+        }
+
+        connection.query(query, (error, filas, campos) => {
+            response.json({ data: filas })
+        })
+
+        connection.release()
+    })
+})
 
 
 module.exports = router
