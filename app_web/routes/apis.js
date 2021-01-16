8e3370313eeea9095fa60d09cfb19a383f60a411
vmedina <vamedinac@icloud.com>
2021-01-15 20:51:26 -0500

    api_publi_id

--- app_web/routes/apis.js
+++ app_web/routes/apis.js
@@ -34,5 +34,24 @@ router.get('/api/v1/publicaciones', (request, response) => {
     })
 })
 
+/**api que me devuelve una publicacion de acuerdo a su id */
+router.get('/api/v1/publicaciones/:id', (request, response) => {
+    pool.getConnection((error, connection) => {
+
+        const query = `select*from publicaciones where id=${connection.escape(request.params.id)}`
+
+        connection.query(query, (error, filas, campos) => {
+            if (filas.length > 0) {
+                response.json({ data: filas[0] })
+            } else {
+                response.status(404)
+                response.send({ errors: ["There is no publication"] })
+            }
+
+        })
+        connection.release()
+    })
+})
+
 
 module.exports = router
