4e7a8b6da1814f9aa6f018ad2988f700c7b168da
Victor Medina <vamedinac@icloud.com>
2022-11-04 21:32:12 -0500

    update code 1

--- app_web/routes/cont_priva.js
+++ app_web/routes/cont_priva.js
@@ -1,51 +1,60 @@
-const express = require('express')
-const router = express.Router()
-const mysql = require('mysql')
+const express = require("express");
+const router = express.Router();
+const mysql = require("mysql");
 
 /**POOL DE CONEXIONES A LA BASE DE DATOS */
 var pool = mysql.createPool({
-    connectionLimit: 20,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'blog_viajes'
-})
+  connectionLimit: 20,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "blog_viajes",
+});
 
 /**RUTA WEB DE USUARIO despues de que SE CONSULTE EN LA BASE DE DATOS SI EL USUARIO ESTA REGISTRADO CARGAMOS LA PAGINA PRIVADA A LA QUE EL USUARIO PUEDE TENER ACCESO */
 
 /**cargamos y sonsultamos los datos de dicho usuario y los mostramos en una tabla html */
-router.get('/admin/index', function (peticion, respuesta) {
-    pool.getConnection(function (err, connection) {
-        const consulta = `
+router.get("/admin/index", function (peticion, respuesta) {
+  pool.getConnection(function (err, connection) {
+    const consulta = `
         SELECT *
         FROM publicaciones
         WHERE
         autor_id = ${connection.escape(peticion.session.usuario.id)}
-      `
-        connection.query(consulta, function (error, filas, campos) {
-            respuesta.render('admin/index', { usuario: peticion.session.usuario, mensaje: peticion.flash('mensaje'), publicaciones: filas })
-        })
-        connection.release()
-    })
-})
+      `;
+    connection.query(consulta, function (error, filas, campos) {
+      respuesta.render("admin/index", {
+        usuario: peticion.session.usuario,
+        mensaje: peticion.flash("mensaje"),
+        publicaciones: filas,
+      });
+    });
+    connection.release();
+  });
+});
 
 /**RUTA PARA CERRAR CESION DIRECCIONAR A LA PAGINA PRICIPAL */
-router.get('/procesar_cerrar_sesion', function (peticion, respuesta) {
-    peticion.session.destroy();
-    respuesta.redirect("/")
+router.get("/procesar_cerrar_sesion", function (peticion, respuesta) {
+  peticion.session.destroy();
+  respuesta.redirect("/");
 });
 
 /**ruta para cargar el formulario donde se agrega un nuevo registro del autor */
-router.get('/admin/agregar', function (peticion, respuesta) {
-    respuesta.render('admin/agregar', { mensaje: peticion.flash('mensaje'), usuario: peticion.session.usuario })
-})
+router.get("/admin/agregar", function (peticion, respuesta) {
+  respuesta.render("admin/agregar", {
+    mensaje: peticion.flash("mensaje"),
+    usuario: peticion.session.usuario,
+  });
+});
 
 /**enviamos el  contenido del formulario con el nuevo registro agregar a la base de datos */
-router.post('/admin/procesar_agregar', function (peticion, respuesta) {
-    pool.getConnection(function (err, connection) {
-        const date = new Date()
-        const fecha = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
-        const consulta = `
+router.post("/admin/procesar_agregar", function (peticion, respuesta) {
+  pool.getConnection(function (err, connection) {
+    const date = new Date();
+    const fecha = `${date.getFullYear()}-${
+      date.getMonth() + 1
+    }-${date.getDate()}`;
+    const consulta = `
             INSERT INTO
             publicaciones
             (titulo, resumen, contenido, autor_id, fecha_hora)
@@ -57,43 +66,46 @@ router.post('/admin/procesar_agregar', function (peticion, respuesta) {
               ${connection.escape(peticion.session.usuario.id)},
               ${connection.escape(fecha)}
             )
-          `
-        connection.query(consulta, function (error, filas, campos) {
-            peticion.flash('mensaje', 'Publicación agregada')
-            respuesta.redirect("/admin/index")
-        })
-        connection.release()
-    })
-})
+          `;
+    connection.query(consulta, function (error, filas, campos) {
+      peticion.flash("mensaje", "Publicación agregada");
+      respuesta.redirect("/admin/index");
+    });
+    connection.release();
+  });
+});
 
 /**ruta con query para editar un parametro de la tabla del autor */
-router.get('/admin/editar/:id', (peticion, respuesta) => {
-    pool.getConnection((err, connection) => {
-      const consulta = `
+router.get("/admin/editar/:id", (peticion, respuesta) => {
+  pool.getConnection((err, connection) => {
+    const consulta = `
         SELECT * FROM publicaciones
         WHERE
         id = ${connection.escape(peticion.params.id)}
         AND
         autor_id = ${connection.escape(peticion.session.usuario.id)}
-      `
-      connection.query(consulta, (error, filas, campos) => {
-        if (filas.length > 0){
-          respuesta.render('admin/editar', {publicacion: filas[0], mensaje: peticion.flash('mensaje'), usuario: peticion.session.usuario})
-        }
-        else{
-          peticion.flash('mensaje', 'Operación no permitida')
-          respuesta.redirect("/admin/index")
-        }
-      })
-      connection.release()
-    })
-  })
+      `;
+    connection.query(consulta, (error, filas, campos) => {
+      if (filas.length > 0) {
+        respuesta.render("admin/editar", {
+          publicacion: filas[0],
+          mensaje: peticion.flash("mensaje"),
+          usuario: peticion.session.usuario,
+        });
+      } else {
+        peticion.flash("mensaje", "Operación no permitida");
+        respuesta.redirect("/admin/index");
+      }
+    });
+    connection.release();
+  });
+});
 
 /**ruta para modificar la fila de la tabla de publicaciones */
 
-  router.post('/admin/procesar_editar', (peticion, respuesta) => {
-    pool.getConnection((err, connection) => {
-      const consulta = `
+router.post("/admin/procesar_editar", (peticion, respuesta) => {
+  pool.getConnection((err, connection) => {
+    const consulta = `
         UPDATE publicaciones
         SET
         titulo = ${connection.escape(peticion.body.titulo)},
@@ -103,24 +115,23 @@ router.get('/admin/editar/:id', (peticion, respuesta) => {
         id = ${connection.escape(peticion.body.id)}
         AND
         autor_id = ${connection.escape(peticion.session.usuario.id)}
-      `
-      connection.query(consulta, (error, filas, campos) => {
-        if (filas && filas.changedRows > 0){
-          peticion.flash('mensaje', 'Publicación editada')
-        }
-        else{
-          peticion.flash('mensaje', 'Publicación no editada')
-        }
-        respuesta.redirect("/admin/index")
-      })
-      connection.release()
-    })
-  })
+      `;
+    connection.query(consulta, (error, filas, campos) => {
+      if (filas && filas.changedRows > 0) {
+        peticion.flash("mensaje", "Publicación editada");
+      } else {
+        peticion.flash("mensaje", "Publicación no editada");
+      }
+      respuesta.redirect("/admin/index");
+    });
+    connection.release();
+  });
+});
 
-  /**ruta para ejecutar el query de eliminacion de registro */
-  router.get('/admin/procesar_eliminar/:id', (peticion, respuesta) => {
-    pool.getConnection((err, connection) => {
-      const consulta = `
+/**ruta para ejecutar el query de eliminacion de registro */
+router.get("/admin/procesar_eliminar/:id", (peticion, respuesta) => {
+  pool.getConnection((err, connection) => {
+    const consulta = `
         DELETE
         FROM
         publicaciones
@@ -128,18 +139,17 @@ router.get('/admin/editar/:id', (peticion, respuesta) => {
         id = ${connection.escape(peticion.params.id)}
         AND
         autor_id = ${connection.escape(peticion.session.usuario.id)}
-      `
-      connection.query(consulta, (error, filas, campos) => {
-        if (filas && filas.affectedRows > 0){
-          peticion.flash('mensaje', 'Publicación eliminada')
-        }
-        else{
-          peticion.flash('mensaje', 'Publicación no eliminada')
-        }
-        respuesta.redirect("/admin/index")
-      })
-      connection.release()
-    })
-  })
+      `;
+    connection.query(consulta, (error, filas, campos) => {
+      if (filas && filas.affectedRows > 0) {
+        peticion.flash("mensaje", "Publicación eliminada");
+      } else {
+        peticion.flash("mensaje", "Publicación no eliminada");
+      }
+      respuesta.redirect("/admin/index");
+    });
+    connection.release();
+  });
+});
 
-module.exports = router
+module.exports = router;
--- app_web/routes/test.js
+++ app_web/routes/test.js
@@ -1,26 +1,14 @@
-const express =require('express')
-const router=express.Router()
-const mysql=require('mysql')
-
-/**POOL DE CONEXIONES A LA BASE DE DATOS */
-var pool = mysql.createPool({
-    connectionLimit: 20,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'blog_viajes'
-})
+const express = require("express");
+const router = express.Router();
 
 /**middleware por aki se monitorea el acceso a la ruta admin */
-router.use('/admin/', (peticion, respuesta, siguiente) => {
-    if (!peticion.session.usuario) {
-      peticion.flash('mensaje', 'Debe iniciar sesión')
-      respuesta.redirect("/inicio")
-    }
-    else {
-      siguiente()
-    }
-  })
+router.use("/admin/", (peticion, respuesta, siguiente) => {
+  if (!peticion.session.usuario) {
+    peticion.flash("mensaje", "Debe iniciar sesión");
+    respuesta.redirect("/inicio");
+  } else {
+    siguiente();
+  }
+});
 
-
-module.exports=router
+module.exports = router;
